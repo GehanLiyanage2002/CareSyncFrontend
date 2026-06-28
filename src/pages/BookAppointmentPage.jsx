7bb9af93e37bef0d44ebbfe4825492a81a348e45
pubudugunawardhana23 <pubudugunawardhana23@gmail.com>
2026-06-28 11:03:11 +0530

    feat: initialize core frontend pages for user registration, authentication, appointment booking, and dashboards

--- src/pages/BookAppointmentPage.jsx
+++ src/pages/BookAppointmentPage.jsx
@@ -451,7 +451,6 @@ const BookAppointmentPage = () => {
                     <option value="">Select Gender</option>
                     <option value="Male">Male</option>
                     <option value="Female">Female</option>
-                    <option value="Other">Other</option>
                   </select>
                   {errors.gender && <p className="text-rose-500 text-xs mt-1">{errors.gender}</p>}
                 </div>

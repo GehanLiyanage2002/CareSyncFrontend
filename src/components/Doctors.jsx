290274f47f2760a2b8d0b0fea8283f3d99a04bd0
AmashaSamarakoon02 <amashasamarakoon4@gmail.com>
2026-06-30 17:17:44 +0530

    feat: append cache-busting timestamps to doctor images and refresh doctor list on every mount

--- src/components/Doctors.jsx
+++ src/components/Doctors.jsx
@@ -17,10 +17,8 @@ const Doctors = ({ onBookNow, hideHeader, defaultSearchTerm = '' }) => {
   const scrollRef = useRef(null);
 
   useEffect(() => {
-    if (!doctors || doctors.length === 0) {
-      dispatch(fetchDoctors());
-    }
-  }, [dispatch, doctors?.length]);
+    dispatch(fetchDoctors());
+  }, [dispatch]);
 
   useEffect(() => {
     const handleAvailability = (data) => dispatch(updateDoctorAvailability(data));
